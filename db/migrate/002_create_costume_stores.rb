3d238939273b1a80c0e21222e90bb9023408b173
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-14 16:51:13 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,10 +1,11 @@
-  class CreateCostumeStore < ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name
-      t.string :size
-      t.string :image_url
-      t.float :price
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :
 
       t.timestamps null: false
     end
